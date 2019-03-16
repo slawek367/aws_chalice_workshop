652f40e131e283549b046a566f8871d36fca9771
slawek367 <slawomir.bando@gmail.com>
2019-03-16 16:17:39 +0100

    Finished first part of todo project

--- mytodo/app.py
+++ mytodo/app.py
@@ -74,3 +74,17 @@ def add_new_todo():
 @app.route('/todos/{uid}', methods=['GET'])
 def get_todo(uid):
     return get_app_db().get_item(uid)
+
+@app.route('/todos/{uid}', methods=['DELETE'])
+def delete_todo(uid):
+    return get_app_db().delete_item(uid)
+
+@app.route('/todos/{uid}', methods=['PUT'])
+def update_todo(uid):
+    body = app.current_request.json_body
+    get_app_db().update_item(
+        uid,
+        description=body.get('description'),
+        state=body.get('state'),
+        metadata=body.get('metadata')
+    )
